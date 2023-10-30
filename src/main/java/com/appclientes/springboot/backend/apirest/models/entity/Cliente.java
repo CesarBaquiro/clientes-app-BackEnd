953d2ede60565df351ea41c52bb87458770bde71
CesarBaquiro <cmbm.kf@gmail.com>
2023-10-30 17:37:55 -0500

    Se configuro la zona horaria

--- src/main/java/com/appclientes/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/appclientes/springboot/backend/apirest/models/entity/Cliente.java
@@ -8,12 +8,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
+//import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -37,14 +38,16 @@ public class Cliente implements Serializable{
 	@Column(nullable=false, unique=true)
 	private String email;
 	
+	@NotNull(message = "La fecha no puede estar vacia")
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 	
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
+//	@PrePersist
+//	public void prePersist() {
+//		createAt = new Date();
+//	}
+	
 	public Long getId() {
 		return id;
 	}
